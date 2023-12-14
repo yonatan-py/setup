0d20b0f155797b7fe730775ae726e9a73898ec63
yoni <yonatan.py@gmail.com>
2023-12-14 11:52:48 +0200

    add the google gpg and apt install google-cloud-cli

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
-mkdir ~/github
+# intall git and clone the repo into ~/github/setup
+
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg \
+    | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 
 sudo apt update
-sudo apt install -y terminator copyq plocate
+
+sudo apt install -y terminator copyq plocate apt-transport-https \
+                    ca-certificates gnupg curl google-cloud-cli
 sudo snap install --classic code intellij-idea-community goland
 sudo snap install android-studio
 
