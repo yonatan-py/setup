94bcab632cc6a644903ce6bb09aa7f9c2d0b0976
yoni <yonatan.py@gmail.com>
2023-12-14 10:59:18 +0200

    added some jetbrains

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
-sudo snap install --classic code 
+sudo apt update
+sudo snap install --classic code intellij-idea-community goland
 sudo apt install -y terminator git copyq
+sudo snap install android-studio
 
